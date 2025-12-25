ef85a57ca17e642e18a558a308bc50b44e3f8790
ShubhamUjgare <shubham.ujgare999@gmail.com>
2025-12-25 13:20:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -134,3 +134,14 @@ function setupAutocomplete(inputId, boxId) {
 // INIT AUTOCOMPLETE
 setupAutocomplete("pickup", "pickupSuggestions");
 setupAutocomplete("destination", "destinationSuggestions");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("mobileMenu");
+
+  if (!hamburger || !menu) return;
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+});
+
